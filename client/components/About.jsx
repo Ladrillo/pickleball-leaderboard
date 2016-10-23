11809d1442fd7abbe0d9d0ec0d235f3b5eecb4e0
Gabriel Cabrejas Poole <gabriel@cabrejas.net>
2016-10-23 12:10:38 -0600

    experimenting with router

--- client/components/About.jsx
+++ client/components/About.jsx
@@ -1,11 +1,36 @@
 import React from 'react';
+import Router from 'react-router/BrowserRouter';
+import Match from 'react-router/Match';
+import Link from 'react-router/Link';
 
-const About = () => {
+
+const About = ({ pathname }) => {
     return (
         <div>
-            ABOUT PAGE
+            <h1>ABOUT PAGE</h1>
             <br/>
-            <a href="/">back home</a>
+
+            <a href = '/'>back home</a><br/>
+            <a href = '/about'>About</a><br/>
+
+            <Link to = { `${pathname}/consensus` }>Consensus</Link><br/>
+            <Link to = { `${pathname}/dev_team` }>Dev Team</Link><br/>
+            <Link to = { `${pathname}/fish_tank` }>Fish Tank</Link>
+
+            <hr/>
+
+            <Match pattern = { `${pathname}/:topicId` } component = { Topic } />
+            <Match exactly pattern = { `${pathname}` } render = { () => <h2>Select Topic</h2> } />
+        </div>
+    );
+};
+
+
+const Topic = ({ params }) => {
+
+    return (
+        <div>
+            <h2>{ params.topicId }</h2>
         </div>
     );
 };
